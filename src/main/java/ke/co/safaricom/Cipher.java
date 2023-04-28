a71084c18855ba38c6e00539a652b00e530af02a
DEBORAH72 <113166185+DEBORAH72@users.noreply.github.com>
2023-04-29 00:45:05 +0300

    adding of the caesar cipher code of strings and method of string build

--- src/main/java/ke/co/safaricom/Cipher.java
+++ src/main/java/ke/co/safaricom/Cipher.java
@@ -1,7 +1,54 @@
 package ke.co.safaricom;
+import java.util.Scanner;
+public class Cipher {
+    // This rotates a character k-positions
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private String mText;
+    private int mShift;
+
+    public Cipher(String mText, int mShift) {
+        this.mText = mText;
+        this.mShift = mShift;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public int getShift() {
+        return mShift;
+    }
+
+    public static char myCipher(char c, int k) {
+        //Here we declare some helping constants
+
+        final int alphaLength = 26;
+        final char asciiShift = Character.isUpperCase(c) ? 'A' : 'a';
+        final int cipherShift = k % alphaLength;
+
+        // we shift down to 0..25 for a..z
+        char shifted = (char) (c + asciiShift);
+
+        // Here we rotate the letter and handle "wrap-around" for negatives and value >= 26
+        shifted = (char) ((shifted - cipherShift - alphaLength) % alphaLength);
+
+        // Here we shift back up to english characters
+        return (char) (shifted + asciiShift);
+    }
+
+    // Rotate a string k-positions
+    public String myCipher() {
+        StringBuilder stringbuilder = new StringBuilder();
+
+        for (int i = 0; i < mText.length(); i++) {
+            if (mText.charAt(i) == ' ') {
+                stringbuilder.append(" ");
+            } else {
+                stringbuilder.append(myCipher(mText.charAt(i), mShift));
+            }
+        }
+        return stringbuilder.toString();
     }
 }
+
+
