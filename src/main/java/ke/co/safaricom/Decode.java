83ff3959cc88b0196ea84c1e8742bd92baeeffab
DEBORAH72 <113166185+DEBORAH72@users.noreply.github.com>
2023-06-23 12:26:16 +0300

    adding a comment on the argument in this class

--- src/main/java/ke/co/safaricom/Decode.java
+++ src/main/java/ke/co/safaricom/Decode.java
@@ -31,4 +31,14 @@ public class Decode {public String decodeText;
         return decryptionKey;
     }
 }
-}
+/*private String codedText = "";
+    int decodeKey (int decryptionKey){
+        if (decryptionKey < 0  ){
+            throw new IllegalArgumentException("Enter encryption key between 0 - 25");
+        } else if (decryptionKey > 25) {
+            throw new IllegalArgumentException("Enter encryption key between 0 - 25");
+        } else {
+            return decryptionKey;
+        }
+    }
+*/
